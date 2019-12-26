cd698767a8551053d172fdb547f8dbe77e627131
sathishprem <nsathishkumar2016>
2019-12-26 09:19:43 +0530

    new project to git

---
+++ src/org/emp/Employees.java
@@ -0,0 +1,31 @@
+package org.emp;
+
+public class Employees {
+public void empId() {
+System.out.println("eid");
+}
+public void empName() {
+System.out.println("enam");
+}
+public void empPhone() {
+System.out.println("ephn");
+}
+public void empDob() {
+System.out.println("edob");
+}
+public void empEmail() {
+System.out.println("email");
+}
+public void empAddress() {
+System.out.println("eads");
+}
+public static void main(String[] args) {
+Employees E=new Employees();
+E.empId();
+E.empName();
+E.empPhone();
+E.empDob();
+E.empEmail();
+E.empAddress();
+}
+}
